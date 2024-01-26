6d9250be60d7cce4f63c29836e91e09146de765b
code0xff <ian.jungyong.um@gmail.com>
2024-01-26 09:40:10 +0900

    feat: Change link removal button style and unordered list button

--- src/lib/editor/editor.svelte
+++ src/lib/editor/editor.svelte
@@ -17,7 +17,7 @@
 	onMount(async () => {
 		const content =
 			localStorage.getItem('auto-saved') ??
-			'<h1>Hello,</h1><p>This is a note web application by <a target="_blank" rel="noopener noreferrer nofollow" href="https://kit.svelte.dev">SvelteKit</a>, <a target="_blank" rel="noopener noreferrer nofollow" href="https://tiptap.dev">Tiptap</a> and <a target="_blank" rel="noopener noreferrer nofollow" href="https://ui.shadcn.com/">shadcn/ui</a>.</p><p>This note app essentially supports three types of headers, basic paragraph type, code block, unordered list, and ordered list types necessary for document creation.</p><p>Header types and basic paragraph types can be emphasized with formatting options such as <strong>bold</strong>, <em>italic</em>, and <s>strike</s>.</p><p>Links can be added or modified through the link button, and they can be removed using the link removal button.</p><p>The history feature is also provided through undo and redo actions.</p><p>Documents are basically stored in <strong>LocalStorage</strong>, and if you wish to save them permanently, you should use the provided save function to store them as HTML files. Additionally, you can upload these files to continue editing.</p><p>The theme supports both Light and Dark modes.</p><p>If you want more features, please visit the tiptap documentation and extend the functionality through extensions.</p><hr><h1><code># H1</code>: H1</h1><h2><code>## H2</code>: H2</h2><h3><code>### H3</code>: H3</h3><p><code>`code`</code>: <code>code</code></p><p><code>**bold**</code>: <strong>bold</strong></p><p><code>*italic*</code>: <em>italic</em></p><p><code>~~strike~~</code>: <s>strike</s></p><p><code>- unordered list</code>:</p><ul><li><p>unordered list</p></li></ul><p><code>1. ordered list</code>:</p><ol><li><p>ordered list</p></li></ol><p><code>```code block```</code>:</p><pre><code>```code block```</code></pre><p><code>&gt; block quote</code>:</p><blockquote><p>block quote</p></blockquote><p><code>---</code>:</p><hr><p><code>cmd+z</code>: undo</p><p><code>cmd+shift+z</code>: redo</p>';
+			'<h1>Hello,</h1><p>This is a note web application by <a target="_blank" rel="noopener noreferrer nofollow" href="https://kit.svelte.dev">SvelteKit</a>, <a target="_blank" rel="noopener noreferrer nofollow" href="https://tiptap.dev">Tiptap</a> and <a target="_blank" rel="noopener noreferrer nofollow" href="https://ui.shadcn.com/">shadcn/ui</a>.</p><p>This note app essentially supports three types of headers, basic paragraph type, code block, unordered list, and ordered list types necessary for document creation.</p><p>Header types and basic paragraph types can be emphasized with formatting options such as <strong>bold</strong>, <em>italic</em>, and <s>strike</s>.</p><p>Links can be added or modified through the link button, and they can be removed using the link removal button.</p><p>The history feature is also provided through undo and redo actions.</p><p>Documents are basically stored in <strong>LocalStorage</strong>, and if you wish to save them permanently, you should use the provided save function to store them as HTML files. Additionally, you can upload these files to continue editing.</p><p>The theme supports both Light and Dark modes.</p><p>If you want more features, please visit the tiptap documentation and extend the functionality through extensions.</p><hr><h1><code># H1</code>: H1</h1><h2><code>## H2</code>: H2</h2><h3><code>### H3</code>: H3</h3><p><code>`code`</code>: <code>code</code></p><p><code>**bold**</code>: <strong>bold</strong></p><p><code>*italic*</code>: <em>italic</em></p><p><code>~~strike~~</code>: <s>strike</s></p><p><code>- or * unordered list</code>:</p><ul><li><p>unordered list</p></li></ul><p><code>1. ordered list</code>:</p><ol><li><p>ordered list</p></li></ol><p><code>```code block```</code>:</p><pre><code>```code block```</code></pre><p><code>&gt; block quote</code>:</p><blockquote><p>block quote</p></blockquote><p><code>---</code>:</p><hr><p><code>cmd+z</code>: undo</p><p><code>cmd+shift+z</code>: redo</p>';
 		editor = new Editor({
 			element: element,
 			editorProps: {
@@ -153,7 +153,7 @@
 				on:click={() => editor.chain().focus().toggleBulletList().run()}
 				variant={editor.isActive('bulletList') ? 'default' : 'secondary'}
 				class="mt-0.5 h-6 px-2 text-sm max-md:hidden"
-				>-UL
+				>*UL
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleOrderedList().run()}
@@ -190,7 +190,7 @@
 			<Button
 				on:click={() => editor.chain().focus().unsetLink().run()}
 				disabled={!editor.isActive('link')}
-				class="my-0.5 h-6 px-2 text-sm"
+				class="my-0.5 h-6 px-2 text-sm underline"
 			>
 				<strike>{'L'}</strike>
 			</Button>
