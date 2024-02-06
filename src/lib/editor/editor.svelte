82249f25494f11d88e217d292b1c482dbaaf4407
code0xff <ian.jungyong.um@gmail.com>
2024-02-06 14:26:13 +0900

    feat: Support full menu on all devices

--- src/lib/editor/editor.svelte
+++ src/lib/editor/editor.svelte
@@ -83,7 +83,7 @@
 			element: element,
 			editorProps: {
 				attributes: {
-					class: 'md:mt-16 mt-12 md:w-[708px] md:py-16 md:px-0 md:mx-auto p-4 outline-none'
+					class: 'mt-16 md:w-[708px] md:py-16 md:px-0 md:mx-auto p-4 outline-none'
 				}
 			},
 			extensions: extensions!,
@@ -139,16 +139,16 @@
 {#if editor}
 	<div>
 		<nav
-			class="fixed left-0 top-0 z-10 w-full bg-white py-4 text-center dark:bg-[color:hsl(240,10%,3.9%)] max-lg:px-4 max-lg:text-left max-md:p-2"
+			class="fixed left-0 top-0 z-10 flex w-full flex-row justify-start overflow-x-auto bg-white p-4 dark:bg-[color:hsl(240,10%,3.9%)] lg:justify-center"
 		>
-			<Button on:click={() => clearContent(editor)} class="h-8 px-2 max-lg:hidden"
+			<Button on:click={() => clearContent(editor)} class="mr-0.5 h-8 px-2"
 				><BookPlus class="h-4 w-4" /></Button
 			>
 			<Button
 				on:click={() => editor.chain().focus().toggleBold().run()}
 				disabled={!editor.can().chain().focus().toggleBold().run()}
 				variant={editor.isActive('bold') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Bold class="h-4 w-4" />
 			</Button>
@@ -156,7 +156,7 @@
 				on:click={() => editor.chain().focus().toggleItalic().run()}
 				disabled={!editor.can().chain().focus().toggleItalic().run()}
 				variant={editor.isActive('italic') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Italic class="h-4 w-4" />
 			</Button>
@@ -164,7 +164,7 @@
 				on:click={() => editor.chain().focus().toggleStrike().run()}
 				disabled={!editor.can().chain().focus().toggleStrike().run()}
 				variant={editor.isActive('strike') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Strikethrough class="h-4 w-4" />
 			</Button>
@@ -172,55 +172,55 @@
 				on:click={() => editor.chain().focus().toggleCode().run()}
 				disabled={!editor.can().chain().focus().toggleCode().run()}
 				variant={editor.isActive('code') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Code class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().setParagraph().run()}
 				variant={editor.isActive('paragraph') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Pilcrow class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
 				variant={editor.isActive('heading', { level: 1 }) ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Heading1 class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
 				variant={editor.isActive('heading', { level: 2 }) ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Heading2 class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
 				variant={editor.isActive('heading', { level: 3 }) ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Heading3 class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().setTextAlign('left').run()}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 				variant={editor.isActive({ textAlign: 'left' }) ? 'default' : 'secondary'}
 			>
 				<AlignLeft class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().setTextAlign('center').run()}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 				variant={editor.isActive({ textAlign: 'center' }) ? 'default' : 'secondary'}
 			>
 				<AlignCenter class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().setTextAlign('right').run()}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 				variant={editor.isActive({ textAlign: 'right' }) ? 'default' : 'secondary'}
 			>
 				<AlignRight class="h-4 w-4" />
@@ -228,66 +228,66 @@
 			<Button
 				on:click={() => editor.chain().focus().toggleBulletList().run()}
 				variant={editor.isActive('bulletList') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 				><List class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleOrderedList().run()}
 				variant={editor.isActive('orderedList') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<ListOrdered class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleCodeBlock().run()}
 				variant={editor.isActive('codeBlock') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Braces class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().toggleBlockquote().run()}
 				variant={editor.isActive('blockquote') ? 'default' : 'secondary'}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<TextQuote class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().setHorizontalRule().run()}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<SeparatorHorizontal class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => setLink(editor)}
 				variant={editor.isActive('link') ? 'default' : 'secondary'}
-				class="h-8 px-2"><Link2 class="h-4 w-4" /></Button
+				class="mx-0.5 h-8 px-2"><Link2 class="h-4 w-4" /></Button
 			>
 			<Button
 				on:click={() => editor.chain().focus().unsetLink().run()}
 				disabled={!editor.isActive('link')}
-				class="h-8 px-2 max-lg:hidden"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Link2Off class="h-4 w-4" />
 			</Button>
-			<Button on:click={() => addImage(editor)} class="h-8 px-2"
+			<Button on:click={() => addImage(editor)} class="mx-0.5 h-8 px-2"
 				><ImagePlus class="h-4 w-4" /></Button
 			>
 			<Button
 				on:click={() => editor.chain().focus().undo().run()}
 				disabled={!editor.can().chain().focus().undo().run()}
-				class="h-8 px-2"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Undo class="h-4 w-4" />
 			</Button>
 			<Button
 				on:click={() => editor.chain().focus().redo().run()}
 				disabled={!editor.can().chain().focus().redo().run()}
-				class="h-8 px-2"
+				class="mx-0.5 h-8 px-2"
 			>
 				<Redo class="h-4 w-4" />
 			</Button>
-			<Button on:click={() => download(editor)} class="h-8 px-2"
+			<Button on:click={() => download(editor)} class="mx-0.5 h-8 px-2"
 				><FileDown class="h-4 w-4" /></Button
 			>
 			<input
@@ -298,15 +298,15 @@
 				on:change={() => upload(editor, files)}
 			/>
 			<Button on:click={() => document.getElementById('selectedFile')?.click()} class="h-8 px-2"
-				><FileUp class="h-4 w-4" /></Button
+				><FileUp class="mx-0.5 h-4 w-4" /></Button
 			>
-			<Button on:click={startCollab} class="h-8 px-2">
+			<Button on:click={startCollab} class="mx-0.5 h-8 px-2">
 				<ScreenShare class="h-4 w-4" />
 			</Button>
-			<Button on:click={endCollab} disabled={!provider} class="h-8 px-2"
+			<Button on:click={endCollab} disabled={!provider} class="mx-0.5 h-8 px-2"
 				><ScreenShareOff class="h-4 w-4" /></Button
 			>
-			<Button on:click={toggleMode} class="h-8 px-2"><SunMoon class="h-4 w-4" /></Button>
+			<Button on:click={toggleMode} class="ml-0.5 h-8 px-2"><SunMoon class="h-4 w-4" /></Button>
 		</nav>
 	</div>
 {/if}
