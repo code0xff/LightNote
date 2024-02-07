5996082b0a9355fd3366673431ba83bb3c13c660
code0xff <ian.jungyong.um@gmail.com>
2024-02-07 13:47:03 +0900

    feat: Support text format download

--- src/lib/editor/editor.ts
+++ src/lib/editor/editor.ts
@@ -2,15 +2,26 @@ import type { Editor } from "@tiptap/core";
 import { htmlStyle } from "./constants";
 
 export function download(editor: Editor) {
-  const defaultName = `light_note_${Date.now()}.html`;
-  let download = window.prompt('Please insert file name', defaultName);
-  if (download === null || download.trim().length === 0) {
-    download = defaultName;
+  const fileName = localStorage.getItem('recently-edited') ?? `light_note_${Date.now()}.html`;
+  let download = window.prompt('Please insert file name', fileName);
+  if (download === null) {
     return;
   }
-  const html = editor.getHTML();
+  if ((!download.endsWith('.html') && !download.endsWith('.txt')) || download.slice(0, download.lastIndexOf('.')).trim().length === 0) {
+    window.alert('Invalid file name: e.g. (light_note.html, light_note.txt)');
+    return;
+  }
+
+  let blob: Blob;
+  if (download.endsWith('.html')) {
+    const html = editor.getHTML();
+    blob = new Blob([htmlStyle, html], { type: 'text/html;charset=utf-8' });
+  } else {
+    const txt = editor.getText();
+    blob = new Blob([txt], { type: 'text/plain;charset=utf-8' });
+  }
+
   const element = document.createElement('a');
-  const blob = new Blob([htmlStyle, html], { type: 'text/html;charset=utf-8' });
   element.setAttribute('href', window.URL.createObjectURL(blob));
   element.setAttribute('download', download);
   element.click();
@@ -18,6 +29,7 @@ export function download(editor: Editor) {
 
 export async function upload(editor: Editor, files: FileList) {
   if (files) {
+    localStorage.setItem('recently-edited', files[0].name);
     editor.commands.setContent(await files[0].text());
   }
 }
