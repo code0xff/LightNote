cf386c7e200965ffd4256d929dd6554128486f66
code0xff <ian.jungyong.um@gmail.com>
2025-05-19 11:56:20 +0900

    build: add devtoolsJson to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), devtoolsJson()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
